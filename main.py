c231ffe1a770f61893b8cd3f6e520cfa622c6ac8
norah-salim <142896757+norah-salim@users.noreply.github.com>
2023-08-29 20:58:36 +0300

    تم حذف #write your code here

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-# write your code here
+
+favorite_animals = ('dog', 'cat', 'monkey', 'rabbit')
+print(favorite_animals[1])
+favorite_animals.renovel('monkey')
+print(favorite_animals)
+favorite_animals.append('i love dog')
+print(favorite_animals)
+favorite_animals.append('i love cat')
+print(favorite_animals)
+favorite_animals.append('i love monkey')
+print(favorite_animals)
+favorite_animals.append('i love monkey')
+print(favorite_animals)
+numper = [1,2,3,4,5]
+numper_sun = [0,1,2,3,4,5]
+print(numper_sun)

d780649d89add4972fc8c1d98faa8c05936a6a33
norah-salim <142896757+norah-salim@users.noreply.github.com>
2023-08-30 19:01:52 +0300

    تم تغيير على الحل

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-
-favorite_animals = ('dog', 'cat', 'monkey', 'rabbit')
+favorite_animals =['dog','cat','monkey','rabbit']
+print(favorite_animals)
 print(favorite_animals[1])
-favorite_animals.renovel('monkey')
+favorite_animals.remove('monkey')
 print(favorite_animals)
-favorite_animals.append('i love dog')
+favorite_animals.append('giraffe')
 print(favorite_animals)
-favorite_animals.append('i love cat')
-print(favorite_animals)
-favorite_animals.append('i love monkey')
-print(favorite_animals)
-favorite_animals.append('i love monkey')
-print(favorite_animals)
-numper = [1,2,3,4,5]
-numper_sun = [0,1,2,3,4,5]
-print(numper_sun)
+for animals in favorite_animals:
+    print(f'i love {animals}')
+numbers=[1,2,3,4,5]
+numbers_sum= 0
+for sum in numbers :
+ numbers_sum+= sum
+ print(numbers_sum)
